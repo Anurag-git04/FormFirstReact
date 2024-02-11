8166f04d09e8d1d22ff227851862280c9da78b71
Anurag-git04 <shawanurag18@gmail.com>
2024-02-11 23:12:00 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import React ,{useState} from 'react';
 import './App.css';
 
 function App() {
+
+  // const [firstName, setFirstName] = useState("");
+  // const [lastName, setLastName] = useState("");
+
+  // function changeFirstHandler(event){
+  //   // console.log("First Name: ")
+  //   // console.log(event.target.value)
+  //   setFirstName(event.target.value)
+  // }
+  // function changeLastHandler(event){
+  //   // console.log("Last Name:")
+  //   // console.log(event.target.value)
+  //   setLastName(event.target.value)
+  // }
+
+  const [formData, setFormData] = useState( {firstName:"",lastName:"", email:""} )
+  console.log(formData)
+  function changeHandler(event){
+      setFormData(prevFormData => {
+        return {
+          ...prevFormData,
+          [event.target.name] : [event.target.value]
+        }
+      })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <form action="">
+          <input type="text" placeholder='First Name' onChange={changeHandler} name="firstName" />
+          <br></br>
+          <br></br>
+          <input type="text" placeholder='Last Name' onChange={changeHandler} name='lastName' />
+          <br/>
+          <br/>
+          <input type="email" placeholder='Enter Your Email Here' onChange={changeHandler} name='email' />
+        </form>
+      </div>
   );
 }
 
